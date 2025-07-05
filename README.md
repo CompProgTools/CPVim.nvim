a20dca2c9d2409186c1d0d2f1858a719cf621603
Kashyap Sukshavasi <ksukshavasi@gmail.com>
2025-07-05 12:15:42 -0400

    added rest of main

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # CPVim
 A neovim plugin that allows you to do quick commands for competitive programming
+
+## Usage
+
+### Lazy.nvim
+
+```lua
+{
+  "compprogtools/cpvim.nvim",
+  lazy = false,
+}
+```
+
+### Packer
+
+```lua
+use "compprogtools/cpvim.nvim"
+```
--- lua/cpvim.lua
+++ lua/cpvim.lua
@@ -5,3 +5,20 @@ vim.api.nvim_create_user_command("CPVim", function(opts))
         return
     end
     require("cpvim").load_template(alias)
+end, {
+    nargs = 1,
+    complete = function(_, line)
+        local home = os.getenv("HOME")
+        local path = home .. "/.cpcli/templates"
+        local handle = io.popen("ls " .. vim.fn.shellescape(path))
+        if not handle then return {} end
+        local result = handle:read("*a")
+        handle:close()
+
+        local files = {}
+        for file in string.gmatch(result, "[^\n]+") do
+            table.insert(files, file)
+        end
+        return files
+    end
+}
