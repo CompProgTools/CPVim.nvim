28926cba9b57044e2ab81cbb3ea12267e91401e5
Kashyap Sukshavasi <ksukshavasi@gmail.com>
2025-08-09 22:13:40 -0400

    sepreate set command

--- lua/cpvim/commands/cpvim.lua
+++ lua/cpvim/commands/cpvim.lua
@@ -3,49 +3,61 @@ local M = {}
 local ratings = require("cpvim.commands.ratings")
 local dashboard = require("cpvim.commands.dashboard")
 local templates = require("cpvim.templates")
+local config = require("cpvim.config")
+
 templates.ensure_dir()
+config.load()
 
 vim.api.nvim_create_user_command("CPVim", function(opts)
-  local args = vim.split(opts.args, "%s+")
-  local command = args[1]
-
-  if command == "ratings" then
-    ratings.show_ratings()
-  elseif command == "dashboard" then
-    dashboard.showDashboard()
-  elseif command and command ~= "" then
-    M.load_template(command)
-  else
-    vim.api.nvim_err_writeln("Usage: CPVim <template> | CPVim ratings | CPVim dashboard")
-  end
-end, {
-  nargs = 1,
-  complete = function()
-    return vim.list_extend({ "ratings", "dashboard" }, templates.list())
-  end
-})
-
-vim.api.nvim_create_user_command("CPVimSet", function(opts)
-  local args = vim.split(opts.args, "%s+")
-  if #args < 2 then
-    vim.api.nvim_err_writeln("Usage: CPVimSet <key> <value>")
+  local args = opts.fargs
+  if #args == 0 then
+    vim.api.nvim_err_writeln("Usage: CPVim <command> [args]")
     return
   end
 
-  local key = args[1]
-  local value = table.concat(args, " ", 2)
+  local cmd = args[1]
 
-  if tonumber(value) then
-    value = tonumber(value)
+  -- Subcommands
+  if cmd == "dashboard" then
+    dashboard.showDashboard()
+
+  elseif cmd == "ratings" then
+    ratings.show_ratings()
+
+  elseif cmd == "set" then
+    if #args < 3 then
+      vim.api.nvim_err_writeln("Usage: CPVim set <key> <value>")
+      return
+    end
+    local key = args[2]
+    local value = table.concat(args, " ", 3)
+
+    if tonumber(value) then
+      value = tonumber(value)
+    end
+
+    config.set(key, value)
+    vim.api.nvim_echo({ { ("cpvim: %s = %s"):format(key, value), "Normal" } }, false, {})
+
+  elseif cmd == "reload" then
+    config.load()
+    vim.api.nvim_echo({{ "cpvim: config reloaded", "Normal" }}, false, {})
+
+  elseif cmd == "templates" then
+    local list = templates.list()
+    if #list == 0 then
+      vim.api.nvim_echo({{ "No templates found", "WarningMsg" }}, false, {})
+    else
+      vim.api.nvim_echo({{ "Templates: " .. table.concat(list, ", "), "Normal" }}, false, {})
+    end
+
+  else
+    M.load_template(cmd)
   end
-
-  local config = require("cpvim.config")
-  config.set(key, value)
-  vim.api.nvim_echo({{("cpvim: %s = %s"):format(key, value), "Normal"}}, false, {})
 end, {
-  nargs = "+",
+  nargs = "*",
   complete = function()
-    return vim.tbl_keys(require("cpvim.config").defaults)
+    return { "set", "dashboard", "ratings", "reload", "templates" }
   end
 })
 
